e87102f1ac6cbe2837b8b0bf52df44b781598a99
AdayJar <adajkrutoj@gmail.com>
2025-05-15 01:49:52 +0500

    feat: entry point

--- analytics-service/cmd/main.go
+++ analytics-service/cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"CodeMart/analytics-service/config"
+	"CodeMart/analytics-service/internal/app"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx := context.Background()
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Printf("failed to parse config: %v", err)
+		return
+	}
+
+	application, err := app.New(ctx, cfg)
+	if err != nil {
+		log.Printf("failed to setup application: %v", err)
+		return
+	}
+
+	err = application.Run()
+	if err != nil {
+		log.Printf("failed to run application: %v", err)
+		return
+	}
 }
