a162a118311506bbae010c18c1ac26657eaaa8d3
Warcon01 <warconprogramming@gmail.com>
2025-05-29 03:46:37 +0500

    feat: entry point

---
+++ product-service/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/RakhatLukum/CodeMart/product-service/config"
+	"github.com/RakhatLukum/CodeMart/product-service/internal/app"
+)
+
+func main() {
+	ctx := context.Background()
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Printf("failed to parse config: %v", err)
+		return
+	}
+
+	application, err := app.New(ctx, cfg)
+	if err != nil {
+		log.Printf("failed to setup application: %v", err)
+		return
+	}
+
+	err = application.Run()
+	if err != nil {
+		log.Printf("failed to run application: %v", err)
+		return
+	}
+}
